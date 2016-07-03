f21b9059e9412bffa51974a991497784cd374c6a
billy1kaplan <billy1kaplan@gmail.com>
2016-07-03 18:10:43 -0400

    Potential nuggetry

--- content_script.js
+++ content_script.js
@@ -1,45 +1,46 @@
-walk(document.body)
+walk(document.body);
 
-function walk(node)
-{
+function walk(node) {
 
     var child, next;
 
-    if (node.tag.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea' || node.classList.indexOf('ace_editor') > -1 {
-        return;
-    }
-
-    switch( node.nodeType )
-    {
+    switch (node.nodeType) {
         case 1: // Element
         case 9: // Document
         case 11: // Document fragment
             child = node.firstChild;
-            while( child ){
-                next = child.nextSibling;
+            while (child) {
+                next = child.nextSibling;   
                 walk(child);
-                child = next
+                child = next;
             }
             break;
 
         case 3: // Text node
-            handleText(node)
+            handleText(node);
             break;
+    }
+}
 
-function handleText(textNode)
-{
+function handleText(textNode) {
     var v = textNode.nodeValue;
+    var findDollars = /(?:\$|USD|Dollar)(?:\s)?((\d+\.\d+)|(\d+))/g; // find and replace pleb money with noble nuggs
+    v = v.replace(findDollars, function (match) {
+        if (match.charAt(0) === '\$') {
+            match = match.substr(1);
+        }
+        return nugCorrection(Number(match));
+    });
 
-    var findDollars = /(?:USD|\$|Dollar)(?:\s)?((\d+\.\d+)|(\d+))/g; // find and replace pleb money with noble nuggs
-    v.replace(findDollars, nugCorrection($1))
+    textNode.nodeValue = v;
 
 }
 
-function nugCorrection(val)
-{
+function nugCorrection(val) {
     // calculate real nugg value, some function nuggValue()
     // for testing, 1 nugget = $3
-    var nugVal = 1/3
-    return String(1/3) + " nuggets"
+    var nugVal = 1 / 3;
+    var nuggets = nugVal * val;
+    return String(nuggets.toFixed(2)) + " nuggets";
 }
 
